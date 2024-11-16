7d75a644c0d0b55ab1b11e0b080f017198cf7a32
dantonov <dananto02@gmail.com>
2024-11-16 19:14:15 +0300

    updated doc yml

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,14 @@
 ﻿namespace ConsoleApp1;
+
+/// <summary>
+/// This is Program class.
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// This is a main method.
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
